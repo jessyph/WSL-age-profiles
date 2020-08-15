69438da0ac0d40b85f2f1f7bb8d9da7467a59b8b
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2020-08-15 16:09:15 +0100

    theme function only

--- age-plot.R
+++ age-plot.R
@@ -6,8 +6,7 @@ font_add_google(name = "Merriweather", family = "merriweather")
 font_add_google(name = "Open Sans", family = "open-sans")
 showtext_auto()
 
-create_AgePlot <- function(club_name, club_colour, club_shade) {
-  theme_AgePlot <- function() {
+theme_AgePlot <- function() {
     theme (
     axis.line = element_line(),
     axis.title = element_text(family = "merriweather"),
@@ -20,26 +19,7 @@ create_AgePlot <- function(club_name, club_colour, club_shade) {
     plot.background = element_blank(),
     plot.margin = unit(c(2, 1, 1, 1), "cm"), #sets axis further in
     plot.title = element_text(family = "merriweather", vjust = 1),
-    plot.subtitle = element_text(family = "merriweather", hjust = 0.55, size = 9, face = "bold")
+    plot.subtitle = element_text(family = "merriweather", hjust = 0.57, size = 9, face = "bold")
   )
   }
   
-  ymin <- -50 #fewest number of minutes
-  ymax <- 1400 #max number of minutes
-  xmin <- 16 #lowest player age
-  xmax <- 35 #highest player age
-  
-  ggplot() + xlim(c(xmin, xmax)) + ylim(c(ymin, ymax)) +
-    theme_AgePlot() +
-    scale_y_continuous(name = "Minute's played",
-                       limits = c(ymin, ymax),
-                       expand = c(0,0),
-                       breaks = c(0, 250, 500, 750, 1000, 1200, 1350)) +
-    scale_x_continuous(name = "Age",
-                       limits = c(xmin, xmax),
-                       breaks = seq(xmin, xmax, 2)) +
-    geom_rect(aes(xmin = 25, xmax = 29, ymin = ymin, ymax = ymax),
-              fill = club_shade, alpha = 0.03)
-}
-
-create_AgePlot("Chelsea", "#0E44D3", "#0EFAF7")
