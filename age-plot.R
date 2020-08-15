6a4d0c4cfa37c48084ef2737b7df132b400b452c
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2020-08-15 14:13:33 +0100

    update title font

--- age-plot.R
+++ age-plot.R
@@ -18,7 +18,8 @@ create_AgePlot <- function(club_name, club_colour, club_shade) {
     panel.grid.minor = element_blank(), #removes grid lines
     panel.grid.major = element_line(colour= "light grey", size = 0.1),
     plot.background = element_blank(),
-    plot.margin = unit(c(2, 1, 1, 1), "cm") #sets axis further in
+    plot.margin = unit(c(2, 1, 1, 1), "cm"), #sets axis further in
+    plot.title = element_text(family = "merriweather")
   )
   }
   
