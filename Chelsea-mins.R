a09da29b6fcb5644abed949355aee6a48a2ec7e0
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2020-08-16 19:12:46 +0100

    update to %

--- Chelsea-mins.R
+++ Chelsea-mins.R
@@ -7,42 +7,42 @@ font_add_google(name = "Merriweather", family = "merriweather")
 font_add_google(name = "Open Sans", family = "open-sans")
 showtext_auto()
 
-Chelsea$new.player <- c("0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "1")
-Chelsea[23, 5] = "0"
-
-ymin <- -50 #fewest number of minutes
-ymax <- 1400 #max number of minutes
-xmin <- 16 #lowest player age
+ymin <- -5 #fewest number of minutes
+ymax <- 105 #max number of minutes
+xmin <- 15 #lowest player age
 xmax <- 35 #highest player age
 
 club_colour = "#0E44D3"
 club_shade = "#0EFAF7"
 club_name = "Chelsea"
 
-ggplot(data = Chelsea, aes(x = Age, y = Minutes.played)) + 
+chelsea <- Minutes.played.1920 %>% filter(team.id=="6")
+
+chelsea <- chelsea %>% mutate_at(vars(mp.percentage), funs(round(., 0)))
+
+ggplot(data = chelsea, aes(x = age, y = mp.percentage)) + 
   xlim(c(xmin, xmax)) + ylim(c(ymin, ymax)) +
   geom_point(colour = club_colour) +
-  geom_text_repel(data = Chelsea %>%
-                    filter(new.player=="0"),
-                  aes(label = Player),
+  geom_text_repel(data = chelsea %>% filter(new.signing=="0"),
+                  aes(label = player),
                   size = 3, family = "open-sans",
                   point.padding = 0.2) +
-  geom_text_repel(data = Chelsea %>%
-                    filter(new.player=="1"),
-                  aes(label = Player),
+  geom_text_repel(data = chelsea %>%
+                    filter(new.signing=="1"),
+                  aes(label = player),
                   size = 3, family = "open-sans", fontface = "bold",
                   point.padding = 0.2) +
   theme_AgePlot() +
-  scale_y_continuous(name = "Minutes played (19/20 season)",
+  scale_y_continuous(name = "% Minutes played (19/20 season)",
                      limits = c(ymin, ymax),
                      expand = c(0,0),
-                     breaks = c(0, 250, 500, 750, 1000, 1200, 1350)) +
+                     breaks = seq(0, 100, 10)) +
   scale_x_continuous(limits = c(xmin, xmax),
                      breaks = seq(xmin, xmax, 2)) +
   geom_rect(aes(xmin = 25, xmax = 29, ymin = ymin, ymax = ymax),
             fill = club_shade, alpha = 0.01) +
   labs(title = paste0(club_name, " squad profile 2020/21 season"),
-       subtitle = paste0("Average age: ", round(mean(Chelsea$Age), 0)),
+       # subtitle = paste0("Average age: ", round(mean(Chelsea$Age), 0)),
        caption = c("New signings in bold \n Peak years shaded",
        "Jessy Parker Humphreys | @jessyjph \n Source: FBRef"))
 
